36e0478ebb90e11fc159cc796f5c1d94a14fd5f7
Shubham Vishwakarma <shubhamvishwakarma0604@gmail.com>
2023-06-28 21:49:36 +0530

    Added Cart Features to Header

--- src/app/header/cart.tsx
+++ src/app/header/cart.tsx
@@ -1,26 +1,33 @@
 "use client";
 import styles from "./header.module.css";
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-async function getCart() {
-  const cart = await fetch("/api/account/cart", {
+async function getCart(cartId: number, setCartLength: Function) {
+  const cartAPI = await fetch("/api/cart", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ id: Session.user.cartId }),
-  }).then((res) => res.json());
-  return cart.length;
+    body: JSON.stringify({ id: cartId }),
+  });
+  const cart = await cartAPI.json();
+  setCartLength(cart.length);
 }
-export default function cart() {
+
+export default function cart(): React.ReactNode {
   const { data: Session, status } = useSession();
-  let cartLength = 0;
-  if (status == "authenticated") {
-    const cartLength = getCart();
-  }
+  const [cartLength, setCartLength] = useState(0);
+  useEffect(() => {
+    if (status == "authenticated") {
+      getCart(Session.user.cartId, setCartLength);
+    } else {
+      setCartLength(0);
+    }
+  }, [status]);
   return (
     <div className={styles.cart}>
       <AiOutlineShoppingCart size={25} />
       <div className={styles.cartItem}>
-        <p>{cartLength ? cartLength : "0"}</p>
+        <p>{cartLength}</p>
       </div>
     </div>
   );
