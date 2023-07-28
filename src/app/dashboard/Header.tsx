e6b0cccbf769d11ddb2e1e4192179e94c43cfbd3
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-07-28 21:06:08 +0530

    Fixed Button Not Clicking Issue in Dashboard

--- src/app/dashboard/Header.tsx
+++ src/app/dashboard/Header.tsx
@@ -3,15 +3,15 @@ import styles from "./dashboard.module.css";
 export default function Account() {
   return (
     <div className={styles.sideBarMenu}>
-      <li>
-        <Link href="/dashboard">Dashboard</Link>
-      </li>
-      <li>
-        <Link href="/dashboard/profile">Profile</Link>
-      </li>
-      <li>
-        <Link href="/dashboard/orders">Orders</Link>
-      </li>
+      <Link href="/dashboard">
+        <li>Dashboard</li>
+      </Link>
+      <Link href="/dashboard/profile">
+        <li>Profile</li>
+      </Link>
+      <Link href="/dashboard/orders">
+        <li>Orders</li>
+      </Link>
     </div>
   );
 }
