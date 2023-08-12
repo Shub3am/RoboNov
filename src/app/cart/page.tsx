a336209dd840a362d706be07450f2a103360c12f
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-08-12 22:34:44 +0530

    working on cart bill

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -9,6 +9,13 @@ import styles from "./cart.module.css";
 //   return getCartData;
 // }
 
+async function getLocalTax(latitude, longitude) {
+  let tax = await fetch(
+    `https://api.precisely.com/localtax/v1/tax/General/bylocation?latitude=${latitude}&longitude=${longitude}&purchaseAmount=100`
+  ).then((x) => x.json());
+  console.log(tax);
+}
+
 export default function CART() {
   const { data: Session, status } = useSession();
   const [cartData, setCart] = useState([]);
@@ -28,14 +35,26 @@ export default function CART() {
     }
   }, [status]);
   if (status == "authenticated" && cartData.length) {
+    let total: { amount: Number; items: Number } = { amount: 0, items: 0 };
+    let tax = 0;
+    navigator.geolocation.getCurrentPosition(async function (location) {
+      await getLocalTax(location.coords.latitude, location.coords.longitude);
+    });
     let cartTable = cartData.map(
       (
         item,
         index
       ): {
-        item: { productid: Number; productname: String; qty: Number };
+        item: {
+          productid: Number;
+          productname: String;
+          qty: Number;
+          productprice: Number;
+        };
         index: Number;
       } => {
+        total.amount = total.amount + item.qty * item.productprice;
+        total.items = total.items + item.qty;
         return (
           <tr key={index}>
             <th>{item.productid}</th>
@@ -61,7 +80,30 @@ export default function CART() {
             {cartTable}
           </table>
         </div>
-        <div></div>
+        <div className={styles.productBill}>
+          <div className={styles.productBillContainer}>
+            <table>
+              <tbody>
+                <tr>
+                  <th>Order Details</th>
+                </tr>
+                <tr>
+                  <th>Totat Items:</th>
+                  <td>{total.items}</td>
+                </tr>
+                <tr>
+                  <th>Total Amount:</th>
+                  <th>Taxes</th>
+                  <td>{tax}</td>
+                  <td>{total.amount}$</td>
+                </tr>
+              </tbody>
+            </table>
+            <div className={styles.orderButton}>
+              <button>Place Order</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   } else {
